3a7d3aa407de788eec87bf745f3c3295ce23944b
Cj-bc <cj-bc@y-modify.org>
2019-01-27 00:53:02 +0900

    Change: remaking

--- libyaml.oo.sh
+++ libyaml.oo.sh
@@ -12,11 +12,19 @@ import util/class
 class:yaml() {
   private string file_name
 
+  # yaml::readfile()
+  # read file and build data structure
+  # @param <string filename>
+  yaml::readfile() {
+     #TODO: 再起使ってかける！！
+  }
+
   # yaml::__init__()
   # initialize yaml file object
   # @param <string filename>
   yaml::__init__() {
     this file_name = "$1"
+    yaml::readfile $1
     @return
   }
 
